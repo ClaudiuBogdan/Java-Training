7ddf30de549d099c2acd91c2d7c89335062662e8
Claudiu Constantin Bogdan <bogdan.claudiu93@gmail.com>
2018-06-15 09:50:30 +0300

    Added solution to challenge of day 29: Bitwise AND.

---
+++ hackerrank/thirty_days_of_code/day29/README.md
@@ -0,0 +1,2 @@
+# Day 29: Bitwise AND
+[Problem statement](30-bitwise-and-English.pdf)
---
+++ hackerrank/thirty_days_of_code/day29/SolutionDay29.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class SolutionDay29 {
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int t = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int tItr = 0; tItr < t; tItr++) {
+            String[] nk = scanner.nextLine().split(" ");
+            int n = Integer.parseInt(nk[0]);
+            int k = Integer.parseInt(nk[1]);
+            int maxVal = 0;
+            for(int i=1; i<n; i++){
+                for(int j = i+1; j <= n; j++){
+                    int bitOpResult = i & j;
+                    if(bitOpResult < k && bitOpResult > maxVal){
+                        maxVal = bitOpResult;
+                    }
+                }
+            }
+            System.out.println(maxVal);
+
+        }
+
+        scanner.close();
+    }
+}
