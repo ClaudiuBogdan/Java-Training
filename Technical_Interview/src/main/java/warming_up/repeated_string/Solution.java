40477358d6c59bca1f120c7773aaa1a563fec808
Claudiu Constantin Bogdan <bogdan.claudiu93@gmail.com>
2019-03-09 20:14:54 +0200

    Completed solution.

--- Technical_Interview/src/main/java/warming_up/repeated_string/Solution.java
+++ Technical_Interview/src/main/java/warming_up/repeated_string/Solution.java
@@ -2,15 +2,23 @@ package warming_up.repeated_string;
 
 public class Solution {
     public static void main(String[] args) {
-        int charOcc = calculateCharOccurence('l', "Hellow World");
+        long charOcc = calculateCharOccurence('l', "Hellow World");
         System.out.println(charOcc);
     }
 
-    public static int calculateCharOccurence(char character, String stringData) {
-        int countOccurence = 0;
+    public static long calculateCharOccurence(char character, String stringData) {
+        long countOccurence = 0;
         for (char stringCharacter : stringData.toCharArray()) {
             countOccurence += character == stringCharacter ? 1 : 0;
         }
         return countOccurence;
+    }
+
+    public static long calculateTotalOccurrence(String substringInput, long numberOfCharacters) {
+        char repitedCharacter = 'a';
+        long characterSubstringCount = calculateCharOccurence(repitedCharacter, substringInput);
+        long remainderSubstringCount = calculateCharOccurence(repitedCharacter, substringInput.substring(0, (int)(numberOfCharacters % substringInput.length())));
+        characterSubstringCount = characterSubstringCount * (numberOfCharacters / substringInput.length()) + remainderSubstringCount;
+        return characterSubstringCount;
     }
 }
--- Technical_Interview/src/test/java/warming_up/repeated_string/SolutionTest.java
+++ Technical_Interview/src/test/java/warming_up/repeated_string/SolutionTest.java
@@ -7,28 +7,59 @@ public class SolutionTest {
 
     @Test
     public void testBeginingString() {
-        Solution tester = new Solution();
         assertEquals(3, Solution.calculateCharOccurence('a', "aaabb"));
         
     }
 
     @Test
     public void testEndString() {
-        Solution tester = new Solution();
         assertEquals(3, Solution.calculateCharOccurence('a', "bbaaa"));
         
     }
 
     @Test
     public void testMiddleString() {
-        Solution tester = new Solution();
         assertEquals(3, Solution.calculateCharOccurence('a', "baaab"));
         
     }
 
     @Test
     public void testContHello() {
-        Solution tester = new Solution();
         assertEquals(3, Solution.calculateCharOccurence('l', "Hellow World"));
+    }
+
+    @Test
+    public void testOnlySubstring() {
+        assertEquals(1, Solution.calculateTotalOccurrence("abc", 3));
+    }
+
+    @Test
+    public void testOnlySubstringOneLetter() {
+        assertEquals(1, Solution.calculateTotalOccurrence("a", 1));
+    }
+
+    @Test
+    public void testSubstringRepitedEntirely() {
+        assertEquals(2, Solution.calculateTotalOccurrence("abc", 6));
+    }
+
+    @Test
+    public void testSubstringRepitedEntirelyTwoOcc() {
+        assertEquals(4, Solution.calculateTotalOccurrence("aba", 6));
+    }
+
+    @Test
+    public void testSubstringRepitedEntirelyOneChar() {
+        assertEquals(2, Solution.calculateTotalOccurrence("a", 2));
+    }
+
+    @Test
+    public void testSubstringRepitedNotEntirely() {
+        assertEquals(2, Solution.calculateTotalOccurrence("abc", 5));
+    }
+
+    @Test
+    public void testSubstringRepitedNotEntirelyTwoLetter() {
+        assertEquals(3, Solution.calculateTotalOccurrence("aba", 5));
     }
 }
